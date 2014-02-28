e09a02826acd10a937c2dbd88d4daee39e001f76
David Greisen <david.greisen@cfpb.gov>
2014-02-28 11:03:30 -0500

    install nginx; confirm elasticsearch running
    install nginx, add config file
    
    removed git river completely
    
    now wait until elastic search has started to install schema.

--- provision.py
+++ provision.py
@@ -4,6 +4,7 @@ from os.path import join
 from os import path
 from subprocess import call, Popen
 import settings
+import time
 
 DIR = path.dirname(path.realpath(__file__))
 
@@ -15,22 +16,36 @@ try:
     call('sudo apt-get install curl openjdk-7-jre-headless -y'.split())
     # install python developer packages
     call('sudo apt-get install python-dev python-pip libxml2-dev libxslt1-dev -y'.split())
-    pass
+    # install nginx
+    call('sudo apt-get install nginx'.split())
 except OSError:
     # if apt isn't installed, need to run yum commands
     call('sudo yum update -y'.split())
     #install java
     call('sudo yum install java-1.7.0-openjdk-devel -y'.split())
-    # install python dev packages
-    call('sudo yum install gcc python-pip python26-devel.x86_64 libxslt-devel.x86_64 libxslt-python.x86_64 libxml2-devel.x86_64 libxml2-python.x86_64 -y')
+    # install dev packages
+    call('sudo yum install git gcc python-pip python26-devel.x86_64 libxslt-devel.x86_64 libxslt-python.x86_64 libxml2-devel.x86_64 libxml2-python.x86_64 -y')
+    # add EPEL repo
+    call('sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'.split())
+    # install nginx
+    call('sudo yum install -y nginx'.split())
+
+# copy over nginx config file
+    with open(join(DIR, 'nginx.conf.template'), 'r') as conf_file:
+        nginx_conf = conf_file.read() % DIR
+    with open('/etc/nginx/nginx.conf', 'w') as conf_file:
+        conf_file.write(nginx_conf)
+
+# start nginx
+call('sudo service nginx start'.split())
+
+# start on startup
+call('sudo chkconfig nginx on'.split())
 
 #install elastic search
 if 'elasticsearch-1.0.0.tar.gz' not in os.listdir(DIR):
     call(['wget', 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.0.tar.gz'])
     call(['tar', '-xvzf', join(DIR, 'elasticsearch-1.0.0.tar.gz')])
-
-    # install git river
-    # call([join(DIR, 'elasticsearch-1.0.0/bin/plugin')] + '--install com.bazoud.elasticsearch/elasticsearch-river-git/0.0.3'.split())
 
 # start elasticsearch
 Popen(join(DIR, 'elasticsearch-1.0.0/bin/elasticsearch'))
@@ -38,9 +53,20 @@ Popen(join(DIR, 'elasticsearch-1.0.0/bin/elasticsearch'))
 # install python dependencies
 call('sudo pip install -r requirements.txt'.split())
 
-# create wiki index with schema
 import requests
 
+# wait until elasticsearch is started up
+while True:
+    print("waiting for elasticsearch to start...")
+    try:
+        requests.get(settings.ES_HOST)
+    except requests.exceptions.ConnectionError:
+        time.sleep(2)
+    else:
+        break
+print("elastic search started")
+
+# create wiki index with schema
 with open(join(DIR, 'wiki_page_schema.json')) as f:
     schema = f.read()
 
