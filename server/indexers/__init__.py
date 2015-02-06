c54e8c2ee6920fd336528d37d4b1daa625392554
David Greisen <david.greisen@cfpb.gov>
2015-02-06 10:13:25 -0500

    refactored indexing
    index.py now just calls index on autocomplete and indexers (soon
    to be renamed search). Jira indexing now performed.
    
    split autocomplete indexing into its own workflow.

--- server/indexers/__init__.py
+++ server/indexers/__init__.py
@@ -1,5 +1,24 @@
-from wiki import index as wiki
-from readme import index as readme
-from gh_pages import index as gh_pages
-from jira import index_all_jira_issues as jira_issues
-from gh_issues import index as gh_issues
+import gevent
+from gh import index as gh
+from ghe import index as ghe
+from jira import index_all_jira_issues as jira
+import urllib3
+from gevent import monkey
+from gevent.pool import Pool
+
+# patches stdlib (including socket and ssl modules) to cooperate with other greenlets
+monkey.patch_all()
+
+def make_pools():
+    pool = Pool(50)
+    pages_pool = urllib3.PoolManager(20)
+    return pool, pages_pool
+
+
+def index():
+    pool, pages_pool = make_pools()
+    gh_jobs, gh_repos = gh(pool, pages_pool)
+    ghe_jobs, ghe_repos = ghe(pool, pages_pool)
+    gevent.joinall(gh_jobs + ghe_jobs)
+    jira_users, jira_projs = jira()
+    return gh_repos, ghe_repos, jira_users, jira_projs
